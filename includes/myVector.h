5ae67c4f7e92e979724901563188858daed071d6
vidaling <vidalgabriel93@gmail.com>
2020-06-08 04:48:40 -0300

    There're still problems with the resize fx that I couldn't solve

--- includes/myVector.h
+++ includes/myVector.h
@@ -11,11 +11,11 @@ class myVector
 {
     private:
         int size,maxsize;
-        int *array;
+        T *array;
     public:
         myVector();
         myVector(int );
-        myVector(const myVector & );
+        myVector(const myVector<T> & );
         ~myVector();
         void resize(int );
         int getsize();
@@ -41,7 +41,7 @@ myVector<T>::myVector(int size)
 }
 
 template<typename T>
-myVector<T>::myVector(const myVector & vec)
+myVector<T>::myVector(const myVector<T> & vec)
 {
 }
 
@@ -68,16 +68,16 @@ bool myVector<T>::operator!=(myVector<T> a)
 template<typename T>
 T & myVector<T>::operator[](int index)
 {
-  if ((index - 1) > this->maxsize) {
+  if ((index - 1) >= this->maxsize) {
     resize(index + 1);
   }
   return this->array[index]; // returned as a reference
 }
-
+/*
 template<typename T>
 void myVector<T>::resize(int newSize)
 {
-  int *temp;
+  T *temp;
   temp = new T [newSize];
   for (int i = 0; i < (newSize); i++) {
      temp[i] = this->array[i];
@@ -86,6 +86,27 @@ void myVector<T>::resize(int newSize)
   this->array = temp;
   this->maxsize = newSize;
 }
+*/
+
+template<typename T>
+void myVector<T>::resize(int newSize)
+{
+  T *temp;
+  temp = new T [newSize];
+
+  if(newSize >= this->maxsize){
+    for (int i = 0; i < this->maxsize; i++) 
+      temp[i] = this->array[i]; 
+  }
+  else{
+    for (int i = 0; i < newSize; i++) 
+      temp[i] = this->array[i];
+  }
+  delete[] this->array;
+  this->array = temp;
+  this->maxsize = newSize;
+  }
+
 
 template<typename T>
 int myVector<T>::getsize()
--- src/main.cpp
+++ src/main.cpp
@@ -24,17 +24,44 @@ int main(int argc, const char** argv)
     }
     cout <<  endl;
     /* Another test, test matrix with operator [][] */
-    myVector<myVector<int> > Matrix;
-
-    Matrix.resize(3);
+   myVector<myVector<int> > Matrix;
+    
+    Matrix.resize(4);
+    
+//    cout << Matrix.getsize() << endl;
     int colsize = Matrix.getsize();
-    for (size_t i = 0; i < colsize; i++)
-    {
-        Matrix[i].resize(3);
+
+   cout << colsize << endl;
+
+    for(int i = 0; i < colsize; i++){
+//        cout<<"hola2"<<endl;
+        Matrix[i].resize(10);
+        int filsize = Matrix[i].getsize();
+         for(int j=0; j < filsize; j++){
+//             cout<<"hola3 ";
+            Matrix[i][j]=j;
+            cout << Matrix[i][j] << " ";
+//           cout<<"hola4 ";
+         }cout << endl;
+     }
+
+myVector<myVector<int> > matrix2;
+   matrix2.resize(5);
+
+   int colsize2 = matrix2.getsize();
+
+   cout << colsize2 << "hola5" << endl;
+
+    for (size_t i = 0; i < colsize2; i++)
+    { matrix2[i].resize(2);
+    int filsize2 = matrix2.getsize();
+        for(size_t j=0; i< filsize2; j++){
+        matrix2[i][j]=2;
+        cout << matrix2[i][j] << " ";}
     }
     
     //Matrix[0][0]=1;
-    
+
 
 
     return 0;
